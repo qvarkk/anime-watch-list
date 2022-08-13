b6e1e5149537a0c9fb622f27664732b14e9859ef
qvarkk <xumdes@gmail.com>
2022-08-13 11:18:44 +0500

    Removed bullshit

--- main.js
+++ main.js
@@ -25,29 +25,3 @@ function addAnimeToList() {
 let anime1 = new Anime('haru', 'finished', '12', '12', '9', 'stupid');
 let anime2 = new Anime('natsu', 'watching', '9', '24', '*', 'quite fun');
 myList.push(anime1, anime2);
-
-const listContainer = document.querySelector('#anime-list');
-
-let animeCard = document.createElement('div');
-animeCard.classList.add('anime-card');
-let content = document.createElement('p');
-content.setAttribute('id', 'content');
-
-for (let i = 0; i < 6; i++) {
-  animeCard.appendChild(content.cloneNode(true));
-}
-
-function displayList() {
-  for (let anime of myList) {
-    listContainer.insertBefore(animeCard.cloneNode(true), listContainer.children[0]);
-    let contentParas = document.querySelectorAll('#content');
-    contentParas[0].innerHTML = anime.title;
-    contentParas[1].innerHTML = anime.status;
-    contentParas[2].innerHTML = anime.epWatched;
-    contentParas[3].innerHTML = anime.epAll;
-    contentParas[4].innerHTML = anime.rating;
-    contentParas[5].innerHTML = anime.notes;
-  }
-}
-displayList();
-
